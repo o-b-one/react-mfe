a4487a46965af11bd85ccb6e893abf8a71d009ab
Orel Balilti <orelba93@gmail.com>
2022-11-06 20:19:31 +0200

    fix(pagination): overfetching issue

--- apps/feed/src/app/feed-page/feed-page.tsx
+++ apps/feed/src/app/feed-page/feed-page.tsx
@@ -1,5 +1,5 @@
 import { Card, Input, Typography, CardContent, Chip } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { FeedService } from '../api/feed.service';
 import styles from './feed-page.module.scss';
 const feedService = new FeedService();
@@ -16,12 +16,44 @@ export function FeedPage(props: FeedPageProps) {
   const [tags,setTags] = useState([]as string[]);
   const [quotes, setQuotes] = useState([]) as [Array<{_id: string, content: string, author: string, tags: string[]}>, React.Dispatch<any>];
   const [searchTerm, setSearchTerm] = useState("")
+  const [inprogress, setInprogress] = useState(false);
   let timeout: NodeJS.Timeout;
+  const hasNext = useRef(true);
+
   const filteredQuotes = quotes.filter(q => q.author.toLowerCase().includes(searchTerm) || q.content.toLowerCase().includes(searchTerm));
 
   useEffect(() => {
-    feedService.getFeed(page, limit, tags).then(res => setQuotes(res.results))
+    if(page > 1 && !hasNext.current){
+      return;
+    }
+    setInprogress(true);
+    feedService.getFeed(page, limit, tags)
+      .then(res => {
+        hasNext.current = res.results.length > 0;
+        return res;
+      })
+      .then(res => setQuotes((prev: any[]) => page === 1 ? res.results : ([...prev, ...res.results])))
+      .finally(() => {
+        if(page === 1){
+          window.scrollTo(0,0);
+        }
+        setInprogress(false);
+      });
   }, [page, limit, tags])
+
+  useEffect(() => {
+    if(inprogress){
+      return;
+    }
+    const onScroll = (e: any) => {
+      if (!inprogress && e.target.documentElement.scrollTop /(e.target.documentElement.scrollHeight / 2) > .80){
+        setPage(prev => prev + 1)
+      }
+    };
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [inprogress]);
 
   const searchWithDebounce = (term: string, debounce: number) => {
     clearTimeout(timeout)
@@ -34,27 +66,34 @@ export function FeedPage(props: FeedPageProps) {
     }else{
       setTags(prev => [...prev, tag]);
     }
+    setPage(1);
   }
 
   return (
     <>
-      <Input onKeyUp={e => searchWithDebounce(e.currentTarget.value, 300)} placeholder="Search"/>
-      {tags.map(tag => <Chip label={tag} key={tag} onClick={() => toggleTag(tag)} />)}
-      {filteredQuotes.map(q => 
-      <div className={styles['fact']} key={q._id}>
-        <Card sx={{ minWidth: 275 }}>
-        <CardContent>
-          {q.tags.map(tag => <Chip label={tag} key={tag} onClick={() => toggleTag(tag)} />)}
-          <Typography variant="body2">
-          <i>"{q.content}"</i>
-          </Typography>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          {q.author}
-          </Typography>
-        </CardContent>
-      </Card>
-      </div>
-      )}      
+      {
+      inprogress && page == 1
+      ? 'Loading...'
+      : (<>
+        <Input onKeyUp={e => searchWithDebounce(e.currentTarget.value, 300)} placeholder="Search"/>
+        {tags.map(tag => <Chip label={tag} key={tag} onClick={() => toggleTag(tag)} />)}
+        {filteredQuotes.map(q => 
+        <div className={styles['fact']} key={q._id}>
+          <Card sx={{ minWidth: 275 }}>
+          <CardContent>
+            {q.tags.map(tag => <Chip label={tag} key={tag} onClick={() => toggleTag(tag)} />)}
+            <Typography variant="body2">
+            <i>"{q.content}"</i>
+            </Typography>
+            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            {q.author}
+            </Typography>
+          </CardContent>
+        </Card>
+        </div>
+        )}      
+      </>
+      )}
     </>
   );
 };
